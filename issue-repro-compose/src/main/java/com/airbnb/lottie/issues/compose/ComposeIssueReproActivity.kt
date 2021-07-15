bba89d3250ed86f21fcccaa66e99fbdffb3d7b79
Marek Hlava <mh@cardlay.com>
2021-07-15 16:01:01 +0200

    animateLottieCompositionAsState doesn't respect min progress

--- issue-repro-compose/src/main/java/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
+++ issue-repro-compose/src/main/java/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
@@ -1,10 +1,16 @@
 package com.airbnb.lottie.issues.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieClipSpec
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 class ComposeIssueReproActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +22,18 @@ class ComposeIssueReproActivity : AppCompatActivity() {
 
     @Composable
     fun Content() {
-        LottieAnimation(null)
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.heart))
+        val state = animateLottieCompositionAsState(
+            composition = composition,
+            iterations = 1,
+            clipSpec = LottieClipSpec.Frame(min = 33, max = 42),
+        )
+
+        Log.w("LOTTIE", "progress = ${state.progress}, isAtEnd = ${state.isAtEnd}")
+
+        LottieAnimation(
+            composition = composition,
+            progress = state.progress,
+        )
     }
 }
